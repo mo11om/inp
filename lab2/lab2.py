26f097230e3f68d027235c51487d8de79495ceac
mo1om <mo1om@DELLNB.localdomain>
2022-10-01 13:52:16 +0800

    lab2 revise in nctu

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -8,9 +8,10 @@ from scapy.all import *
 def parse_pcap(pcap_path ):
     pcap_flow = rdpcap(pcap_path)
     for packet in pcap_flow:
-        if packet.ttl >128:
-            print (packet.ttl)
-            print (packet.tos)
+        if packet.ttl >150:
+            print ("tos",packet.tos)
+
+            print ("ttl", packet.ttl)
             print (base64.b64decode (bytes(packet[TCP].payload)))
      
 
